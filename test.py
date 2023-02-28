3544dee15e53eb8f055fac0c511f8fabe1e5b9ac
Jair Guzman <a16003774@alumnos.uady.mx>
2023-02-27 20:32:40 -0600

    Cambio a test

--- test.py
+++ test.py
@@ -5,10 +5,19 @@ df = pd.read_csv(r"C:\Users\fca\Downloads\Herramientas3_2023_banco.csv")
 
 
 # Select the columns you're interested in
-columns_of_interest = ['job', 'marital', 'education', 'default', 'housing', 'loan', 'contact', 'month', 'day_of_week', 'poutcome']
+columns_of_jobs = ['job'], 'marital', 'education', 'default', 'housing', 'loan', 'contact', 'month', 'day_of_week', 'poutcome'
 
 # Get unique values for the selected columns and store them in a new DataFrame
-unique_data = pd.DataFrame(df[columns_of_interest].drop_duplicates())
+job = pd.DataFrame(df['job'].drop_duplicates())
+marital = pd.DataFrame(df['marital'].drop_duplicates())
+education = pd.DataFrame(df['education'].drop_duplicates())
+housing = pd.DataFrame(df['housing'].drop_duplicates())
+loan = pd.DataFrame(df['loan'].drop_duplicates())
+contact = pd.DataFrame(df['contact'].drop_duplicates())
+month = pd.DataFrame(df['month'].drop_duplicates())
+day_of_week = pd.DataFrame(df['day_of_week'].drop_duplicates())
+listPaises = df["job"].unique().tolist()
+
 
 # Print the resulting DataFrame
-print(unique_data)
+print(listPaises)
